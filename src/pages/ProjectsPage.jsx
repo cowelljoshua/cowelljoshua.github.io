9aa9bc323eee7abf5526dc1e17bb485d9b6af6a9
Joshua Cowell <joshcowell@Joshuas-MacBook-Air-290.local>
2026-05-14 00:15:57 -0400

    Add 80% zoom to Projects page with width compensation to fill edges

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,7 +2,7 @@ import Projects from "../components/Projects";
 
 const ProjectsPage = () => {
   return (
-    <div style={{ paddingTop: "80px" }}>
+    <div style={{ paddingTop: "80px", zoom: 0.8, width: "125%", marginLeft: "-12.5%" }}>
       <Projects />
     </div>
   );
